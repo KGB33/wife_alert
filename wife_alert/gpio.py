2ad572c2a970a548bf2c129d15ac56e50ce0674d
KGB33 <github@bassingthwaite.org>
2020-09-08 08:50:44 -0600

    added GPIO stuff

--- wife_alert/gpio.py
+++ wife_alert/gpio.py
@@ -4,8 +4,17 @@ by sending functions to a redis task queue
 """
 import time
 
+import RPi.GPIO as GPIO
+
+
+def _setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LED_PIN, GPIO.OUT, inital=GPIO.LOW)
+
 
 def make_blink():
     for _ in range(5):
-        print("---BLINK---", time.time())
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(LED_PIN, GPIO.LOW)
         time.sleep(0.5)
